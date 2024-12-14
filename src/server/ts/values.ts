7a28b54cc2c02c4f49013004ef657f91e706cc82
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-12-14 17:51:02 -0500

    fix: kms

--- src/server/ts/values.ts
+++ src/server/ts/values.ts
@@ -2,7 +2,7 @@ export const minDate = new Date(
   "2024-12-14T22:40:25.516Z"
 )
 export const maxDate = new Date(
-  " 2024-12-14T22:40:25.516Z"
+  "2024-12-14T22:40:25.516Z"
 )
 export const seconds = (maxDate.getTime() - minDate.getTime()) / 1000
 export const sampleRate = 250

e9d9dab759163c8252fdc338926d4bf3d7b4379e
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-12-14 19:02:07 -0500

    remove unused data

--- data/hf.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_dataset
-
-ds = load_dataset("alonso130r/HTR-2024-brachy")
-# take the first 5 rows
-ds = ds.select(range(5))
-
-# save to csv
-ds.to_csv("data/htr-2024-brachy.csv")
-
